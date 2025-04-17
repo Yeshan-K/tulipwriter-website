f76fe5db0af33eab58d67652d562540d19df4121
Yeshan Kaushik <yeshankaushikyk@gmail.com>
2025-04-18 02:40:51 +0530

    feat: navigation bar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,13 @@
+import NavigationMenu from "components/NavigationMenu/NavigationMenu"
 import "styles/tailwind.css"
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <NavigationMenu />
+        {children}
+      </body>
     </html>
   )
 }
