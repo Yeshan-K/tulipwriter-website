389c89230a25616396bfeb225c681fe0632baa11
Yeshan Kaushik <yeshankaushikyk@gmail.com>
2025-04-21 23:21:41 +0530

    fix: spacing

--- app/page.tsx
+++ app/page.tsx
@@ -25,16 +25,18 @@ export default function Web() {
   return (
     <>
       <section className="bg-appBackground">
-        <div className="mx-auto grid max-w-(--breakpoint-lg) px-4 py-8 pb-1 text-start lg:px-24 lg:py-16 lg:pb-2">
+        <div className="mx-auto grid max-w-(--breakpoint-lg) px-8 py-8 pb-1 text-start md:px-24 lg:py-16 lg:pb-2">
           <div className="mr-auto place-self-center">
             <h1 className="text-appLayoutText mb-4 max-w-2xl text-4xl leading-none font-light tracking-tight md:text-5xl xl:text-6xl">
               Tulip Writer
             </h1>
-            <p className="text-appLayoutTextMuted mb-4 max-w-2xl font-light md:text-lg lg:mb-8 lg:text-xl">Write.</p>
+            <p className="text-appLayoutTextMuted mb-4 max-w-2xl px-1 font-light md:text-lg lg:mb-8 lg:text-xl">
+              Write.
+            </p>
           </div>
         </div>
-        <div className="mx-auto grid max-w-(--breakpoint-lg) lg:grid-cols-6 px-4 lg:px-12 gap-2 mb-4 lg:mb-8 text-center ">
-          <Button href="https://github.com/Blazity/next-enterprise" className=" col-span-2">
+        <div className="mx-auto mb-4 grid max-w-(--breakpoint-lg) gap-2 px-4 text-center md:grid-cols-3 md:px-12 lg:mb-8 lg:grid-cols-6">
+          <Button href="https://github.com/Blazity/next-enterprise" className="col-span-2">
             Download for Windows
           </Button>
           <Button
@@ -67,9 +69,14 @@ export default function Web() {
           </Button>
         </div>
       </section>
-      <section className="bg-appBackground">
-        <div className="mx-auto max-w-(--breakpoint-xl) px-4 py-8 sm:py-16 lg:px-6">
-          <div className="justify-center space-y-8 md:grid md:grid-cols-2 md:gap-12 md:space-y-0 lg:grid-cols-3">
+      <section className="bg-appBackground mb-4 px-4 md:px-12 lg:px-6">
+        <div className="border-appLayoutBorder mx-auto max-w-(--breakpoint-xl) rounded-xl px-4 pt-4 pb-6 lg:px-6">
+          <div className="mr-auto place-self-center">
+            <h2 className="text-appLayoutText mb-5 px-1 max-w-2xl text-3xl leading-none font-light tracking-tight md:text-4xl xl:text-5xl ">
+              Features{" "}
+            </h2>
+          </div>
+          <div className="justify-center space-y-4 md:grid md:grid-cols-2 md:gap-4 md:space-y-0 lg:grid-cols-3 lg:gap-6">
             {LP_GRID_ITEMS.map((singleItem) => (
               <GrainyDiv
                 key={singleItem.title}
