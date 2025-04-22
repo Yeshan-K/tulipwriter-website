851ad9e8d4cd6ccf532b7cd8e9b06c3e202bd8c8
Yeshan Kaushik <yeshankaushikyk@gmail.com>
2025-04-23 01:57:19 +0530

    fix: changed metadata

--- app/page.tsx
+++ app/page.tsx
@@ -7,18 +7,14 @@ import { LP_GRID_ITEMS } from "lp-items"
 import { FeatureView } from "components/FeatureView/FeatureView"
 
 export const metadata: Metadata = {
-  title: "Next.js Enterprise Boilerplate",
+  title: "Tulip Writer",
   twitter: {
     card: "summary_large_image",
   },
   openGraph: {
-    url: "https://next-enterprise.vercel.app/",
+    url: "https://tulipwriter-website.vercel.app/",
     images: [
-      {
-        width: 1200,
-        height: 630,
-        url: "https://raw.githubusercontent.com/Blazity/next-enterprise/main/.github/assets/project-logo.png",
-      },
+      
     ],
   },
 }
@@ -38,33 +34,33 @@ export default function Web() {
           </div>
         </div>
         <div className="mx-auto mb-4 grid max-w-(--breakpoint-lg) gap-2 px-4 text-center md:grid-cols-3 md:px-12 lg:mb-8 lg:grid-cols-6">
-          <Button href="https://github.com/Blazity/next-enterprise" className="col-span-2">
+          <Button href="#" className="col-span-2">
             Download for Windows
           </Button>
           <Button
             className=""
-            href="https://vercel.com/new/git/external?repository-url=https://github.com/Blazity/next-enterprise"
+            href="#"
             intent="secondary"
           >
             MacOS
           </Button>
           <Button
             className=""
-            href="https://vercel.com/new/git/external?repository-url=https://github.com/Blazity/next-enterprise"
+            href="#"
             intent="secondary"
           >
             Linux
           </Button>
           <Button
             className=""
-            href="https://vercel.com/new/git/external?repository-url=https://github.com/Blazity/next-enterprise"
+            href="#"
             intent="secondary"
           >
             Android
           </Button>
           <Button
             className=""
-            href="https://vercel.com/new/git/external?repository-url=https://github.com/Blazity/next-enterprise"
+            href="#"
             intent="secondary"
           >
             iOS
