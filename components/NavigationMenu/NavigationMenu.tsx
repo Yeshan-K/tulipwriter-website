9b1a7a2268d912448a6595ef660cb580563e9da5
Yeshan Kaushik <yeshankaushikyk@gmail.com>
2025-04-23 00:49:02 +0530

    fix: styling

--- components/NavigationMenu/NavigationMenu.tsx
+++ components/NavigationMenu/NavigationMenu.tsx
@@ -9,16 +9,15 @@ export function NavigationMenu() {
 
   return (
     <motion.div className="border-appLayoutBorder mx-auto flex h-fit max-w-(--breakpoint-xl) items-center justify-start rounded-xl border lg:px-4">
-      <nav className="w-full border-gray-200 bg-white dark:bg-gray-900">
+      <nav className="border-appLayoutBorder bg-appBackground w-full">
         <div className="mx-auto flex max-w-screen-xl flex-wrap items-center justify-between p-4">
-          <Link href="https://flowbite.com/" className="flex items-center space-x-3 rtl:space-x-reverse">
-            <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo" />
-            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+          <Link href="#" className="flex items-center space-x-3 rtl:space-x-reverse">
+            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Tulip Writer</span>
           </Link>
           <div className="flex space-x-3 md:order-2 md:space-x-0 rtl:space-x-reverse">
             <button
               type="button"
-              className="rounded-lg bg-blue-700 px-4 py-2 text-center text-sm font-medium text-white hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 focus:outline-none dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              className="text-appLayoutText focus:ring-appLayoutBorder rounded-lg bg-green-900 px-4 py-2 text-center text-sm font-medium hover:bg-green-700 focus:ring-4 focus:outline-none"
             >
               Get started
             </button>
@@ -28,7 +27,7 @@ export function NavigationMenu() {
               onClick={() => {
                 setIsOpen(!isOpen)
               }}
-              className="inline-flex h-10 w-10 items-center justify-center rounded-lg p-2 text-sm text-gray-500 hover:bg-gray-100 focus:ring-2 focus:ring-gray-200 focus:outline-none md:hidden dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+              className="text-appLayoutText hover:bg-appLayoutInverseHover focus:ring-appLayoutBorder inline-flex h-10 w-10 items-center justify-center rounded-lg p-2 text-sm focus:ring-2 focus:outline-none md:hidden"
               aria-controls="navbar-cta"
               aria-expanded="false"
             >
@@ -54,38 +53,14 @@ export function NavigationMenu() {
             className={`${!isOpen && "hidden"} w-full items-center justify-between md:order-1 md:flex md:w-auto`}
             id="navbar-cta"
           >
-            <ul className="mt-4 flex flex-col rounded-lg border border-gray-100 bg-gray-50 p-4 font-medium md:mt-0 md:flex-row md:space-x-8 md:border-0 md:bg-white md:p-0 rtl:space-x-reverse dark:border-gray-700 dark:bg-gray-800 md:dark:bg-gray-900">
+            <ul className="mt-4 flex flex-col rounded-lg border border-appLayoutBorder p-4 font-medium md:mt-0 md:flex-row md:space-x-8 md:border-0 md:p-0 rtl:space-x-reverse">
+     
               <li>
                 <Link
                   href="#"
-                  className="block rounded-sm bg-blue-700 px-3 py-2 text-white md:bg-transparent md:p-0 md:text-blue-700 md:dark:text-blue-500"
-                  aria-current="page"
-                >
-                  Home
-                </Link>
-              </li>
-              <li>
-                <Link
-                  href="#"
-                  className="block rounded-sm px-3 py-2 text-gray-900 hover:bg-gray-100 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent md:dark:hover:text-blue-500"
+                  className="block rounded-sm px-3 py-2 text-appLayoutText hover:bg-appLayoutInverseHover md:p-0 md:hover:bg-transparent "
                 >
                   About
-                </Link>
-              </li>
-              <li>
-                <Link
-                  href="#"
-                  className="block rounded-sm px-3 py-2 text-gray-900 hover:bg-gray-100 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent md:dark:hover:text-blue-500"
-                >
-                  Services
-                </Link>
-              </li>
-              <li>
-                <Link
-                  href="#"
-                  className="block rounded-sm px-3 py-2 text-gray-900 hover:bg-gray-100 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent md:dark:hover:text-blue-500"
-                >
-                  Contact
                 </Link>
               </li>
             </ul>
